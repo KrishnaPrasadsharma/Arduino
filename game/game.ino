f7845778f2982925561c8a40015e0f160d566ce9
KrishnaPrasadsharma <krishnaprdsharma706@gmail.com>
2023-10-10 09:11:48 +0600

    game

---
+++ game/game.ino
@@ -0,0 +1,15 @@
+
+
+void setup(){}
+int multiplication(int number, int factor){
+int product = number * factor;
+return product;
+}
+int addition(int number, int increment){
+int sum = number * increment;  
+return sum;
+}
+void loop(){
+int one = multiplication(3,3);
+int two = addition(3,3);
+}
